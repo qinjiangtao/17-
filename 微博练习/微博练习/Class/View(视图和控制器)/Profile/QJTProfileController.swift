892ebfe3d22667b7e5a45e275fa2d13e1ee7cc64
admin <474552413@qq.com>
2017-11-06 14:59:13 +0800

    ceshi

---
+++ 微博练习/微博练习/Class/View(视图和控制器)/Profile/QJTProfileController.swift
@@ -0,0 +1,35 @@
+//
+//  QJTProfileController.swift
+//  微博练习
+//
+//  Created by admin on 2017/11/6.
+//  Copyright © 2017年 秦江涛. All rights reserved.
+//
+
+import UIKit
+
+class QJTProfileController: QJTBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 微博练习/微博练习/Weibo-Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Weibo-Bridge.h
+//  微博练习
+//
+//  Created by admin on 2017/11/6.
+//  Copyright © 2017年 秦江涛. All rights reserved.
+//
+
+// 桥接文件，专门用于引入 OC 的头文件，一旦引入，Swift 就可以正常使用(宏除外)
+#import "CZAdditions.h"
+
+
+
